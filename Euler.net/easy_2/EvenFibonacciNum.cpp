88030307e29870534531aba560a9f7d5f52b22e2
Larusu <ldaneliee33@gmail.com>
2025-08-14 10:45:06 +0800

    problem solved

--- Euler.net/easy_2/EvenFibonacciNum.cpp
+++ Euler.net/easy_2/EvenFibonacciNum.cpp
@@ -1,11 +1,35 @@
-#include <iostream>
+// Sum of even-valued terms in fibonacci not exceeding 4,000,000
 
-int fibonacci()
+#include <iostream>
+#include <map>
+
+std::map<int, long> memo;
+
+long fibonacci(int n)
 {
-    
+    if(n <= 1) 
+        return n;
+
+    if(memo.count(n))
+        return memo[n];
+
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    return memo[n];
 }
 
 int main()
 {
+    long fib, value = 0, max = 4000000;
 
+    for(int i = 1; fib <= max; i++)
+    {
+        fib = fibonacci(i);
+
+        if(fib >= max)
+            break;
+
+        value += (fib % 2 == 0) ? fib : 0;
+    }
+
+    std::cout << "Sum of even-valued terms: " << value;
 }
